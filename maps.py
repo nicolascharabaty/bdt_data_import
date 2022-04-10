717c31a5c1f2e470a0177586bb1a4f38974289ad
Nicolas Charabaty <nicolas.charabaty@servmeco.com>
2022-04-10 21:44:59 +0300

    Add example keys for mapping the patient

---
+++ maps.py
@@ -0,0 +1,28 @@
+start_line = '80006100'
+
+patient_keys = {
+   '8000':{
+      'type':'str',
+      'label':'id'
+   },
+   '8100':{
+      'type':'str',
+      'label':'sentence_length'
+   },
+   '3110' :{
+       'type': 'str',
+       'label': 'gender'
+   },
+   '3101' : {
+       'type': 'str',
+       'label' : 'name'
+   },
+   '3102' : {
+       'type': 'str',
+       'label' : 'first_name'
+   },
+   '3103' : {
+       'type' : 'date',
+       'label' : 'dob'
+   }
+}
